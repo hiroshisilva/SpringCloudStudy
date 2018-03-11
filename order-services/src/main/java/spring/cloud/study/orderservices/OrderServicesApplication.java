74cd6903543e08913ab329cd008eb16eae071001
Andre Hiroshi da Silva <hiroshisilva@gmail.com>
2018-03-11 17:06:59 -0300

    Create project

---
+++ order-services/src/main/java/spring/cloud/study/orderservices/OrderServicesApplication.java
@@ -0,0 +1,12 @@
+package spring.cloud.study.orderservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServicesApplication.class, args);
+	}
+}
---
+++ products-services/src/main/java/spring/cloud/study/productsservices/ProductsServicesApplication.java
@@ -0,0 +1,12 @@
+package spring.cloud.study.productsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServicesApplication.class, args);
+	}
+}
